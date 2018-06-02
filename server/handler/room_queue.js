6b931f9c51585ae566f3799c99f15ff80ae0e020
서상희 <tbvjaos510@naver.com>
2018-06-02 18:27:34 +0900

    queue 완성

--- server/handler/room_queue.js
+++ server/handler/room_queue.js
@@ -1,10 +1,38 @@
-
 var queuecnt = 0;
+var queuematch = [];
 
 module.exports = {
-    event : 'queue',
-    callback : function(data){
-        console.log('queue ' + data);
-        queuecnt++;
+    push: {
+        event: 'queue',
+        callback: function (data, client) {
+            console.log('queue ' + data);
+            queuematch.push({
+                client: client,
+                name: data
+            });
+            queuecnt++;
+            if (queuecnt % 2 === 0) {
+                queuecnt -= 2;
+                var client_1 = queuematch.shift();
+                var client_2 = queuematch.shift();
+                client_1.client.write(toHangul(`match ${client_2.name}`));
+                client_2.client.write(toHangul(`match ${client_1.name}`));
+                
+                console.log(`match ${client_1.name} and ${client_2.name}`);
+            }
+        }
+    },
+    pop : {
+        event : 'dequeue',
+        callback:function(data, client){
+            console.log('dequeue ' + data);
+            // queuematch.splice(queuematch.filter(function (el){
+            //     return el.client.id !== client.id;
+            // }), 1);
+            queuematch.splice(queuematch.map((data)=>{return data.client.id}).indexOf(client.id), 1);
+            queuecnt--;
+            console.log('delete ' + client.id);
+        }
+
     }
 };
