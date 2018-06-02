75595da49886ce7744df26355847fc882e38aabc
서상희 <tbvjaos510@naver.com>
2018-06-03 01:12:24 +0900

    상대 이름

--- Test/전략줄다리기/main.cpp
+++ Test/전략줄다리기/main.cpp
@@ -25,15 +25,15 @@ void TTF_DrawText(SDL_Renderer* renderer, string text, SDL_Point point, TTF_Font
 	dst.h = src.h;
 	SDL_RenderCopy(renderer, texture, &src, &dst);
 }
-
-
+string user_name;
+string enemy_name;
 void ReceiveHandler(void) {
 	char msg[255] = "";
 	SDL_Event event;
 	char buff[100] = "";
 	int buffint = 0;
 	event.type = SDL_USEREVENT;
-	event.user.code = 8;
+	event.user.code = SOCKET_EVENT;
 	while (recv(server, msg, sizeof(msg), 0) > 0) {
 
 		printf("%s\n", msg);
@@ -77,13 +77,13 @@ int main(void) {
 
 	int Display_X = 1920;
 	int Display_Y = 1080;
-	SDL_Window* window= SDL_CreateWindow("HitMind_2", 0,0, 1920,1080, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("HitMind_2", 0, 0, 1920, 1080, SDL_WINDOW_SHOWN);
 	SDL_Renderer*	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_Event event;
 	string str;
 	TTF_Init();
-	TTF_Font* font =TTF_OpenFont(".\\font\\NanumGothic.ttf", 30);
-	
+	TTF_Font* font = TTF_OpenFont(".\\font\\NanumGothic.ttf", 30);
+
 
 	SDL_Texture *lobiimage = LoadTexture(renderer, ".\\resources\\image\\tema.jpg");
 	SDL_Texture *inputimage = LoadTexture(renderer, ".\\resources\\image\\input.png");
@@ -99,7 +99,7 @@ int main(void) {
 		RenderTextureXYWH(renderer, lobiimage, 0, 0, Display_X, Display_Y);
 		RenderTextureXYWH(renderer, inputimage, 710, 470, 500, 141);
 
-		
+
 
 		switch (event.type) {
 		case SDL_KEYDOWN:
@@ -107,6 +107,7 @@ int main(void) {
 				str.pop_back();
 			else if (event.key.keysym.sym == SDLK_RETURN || event.key.keysym.sym == SDLK_KP_ENTER && !str.empty()) { //엔터입력
 				matching_start(str.c_str());
+				user_name = str;
 				quit = true;
 				gaming = true;
 			}
@@ -118,7 +119,7 @@ int main(void) {
 			break;
 		case SDL_QUIT:
 			if (status == 1)
-			matching_end();
+				matching_end();
 			quit = TRUE;
 			break;
 		default:
@@ -130,7 +131,7 @@ int main(void) {
 
 	}
 
-	
+
 	SDL_Texture *outimage = LoadTexture(renderer, ".\\resources\\image\\out.jpg");
 
 	RenderTextureXYWH(renderer, lobiimage, 0, 0, Display_X, Display_Y);
@@ -139,9 +140,13 @@ int main(void) {
 		SDL_WaitEvent(&event);
 		switch (event.type) {
 		case SDL_USEREVENT:
-			if (event.user.type == MATCHING) {
-				printf("match success\n");
-				//EXITING 이벤트 처리	
+			if (event.user.code = SOCKET_EVENT) {
+				if (event.user.type == MATCHING) {
+					enemy_name = string((char *)event.user.data1);
+					cout << enemy_name << endl;
+					printf("match success\n");
+					//EXITING 이벤트 처리	
+				}
 			}
 			break;
 		case SDL_QUIT:
@@ -168,7 +173,7 @@ int main(void) {
 
 	Entity *BG = new Entity(renderer, "./resources/image/porg.jpg", Rect(0, 0, 640, 480), Rect(0, 0, 640, 480), 4, 0, false, ENTITY_BG);
 
-	Entity *entity = new Entity(renderer, "./resources/image/porg.jpg", Rect(100,100, 100,100),Rect(100,100,100,100), 1, 1, 1);
+	Entity *entity = new Entity(renderer, "./resources/image/porg.jpg", Rect(100, 100, 100, 100), Rect(100, 100, 100, 100), 1, 1, 1);
 	Entity *entity2 = new Entity(renderer, "./resources/image/porg.jpg", Rect(300, 100, 100, 100), Rect(300, 100, 100, 100), 2, 1, 1);
 	Entity *entity3 = new Entity(renderer, "./resources/image/porg.jpg", Rect(300, 300, 100, 100), Rect(300, 300, 100, 100), 3, 1, 1);
 
@@ -177,7 +182,7 @@ int main(void) {
 	entity->RenderEntity();
 	entity2->RenderEntity();
 	entity3->RenderEntity();
-	
+
 	SDL_RenderPresent(renderer);
 	bool quit = false;
 	SDL_Event event;
@@ -202,9 +207,9 @@ int main(void) {
 				SDL_Point p = entity->v.back(); entity->v.pop_back();
 				entity->Animation(p);
 				idQ.push(id);
-			}
+				}
 			entity->RenderEntity();
-		}
+			}
 
 
 
@@ -213,10 +218,10 @@ int main(void) {
 
 		}
 		SDL_RenderPresent(renderer);
-	}
+		}
 
 #endif
 
 
-	
-}
+
+	}
