644dbbd326b03c671dc7f2cb7016bfa548570ca7
ShinSH0 <klkura1@naver.com>
2018-06-02 20:16:46 +0900

    이벤트큐 추가

--- Test/전략줄다리기/main.cpp
+++ Test/전략줄다리기/main.cpp
@@ -4,10 +4,12 @@
 #pragma comment (lib , "lib/SDL2.lib")
 #undef main 
 
+queue<int> idQ;
+
 int main(void) {
 
-	queue<int> idQ;
-	map<int, Entity> entities;
+	
+	map<int, Entity *> entities;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -15,15 +17,31 @@ int main(void) {
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	Entity *entity = new Entity(renderer, "./resources/image/porg.jpg", Rect(100,100, 100,100),Rect(100,100,100,100), 1, 1);
+	Entity *entity = new Entity(renderer, "./resources/image/porg.jpg", Rect(100,100, 100,100),Rect(100,100,100,100), 1, 1, 1);
 
+	entities.insert(make_pair(entity->id, entity));
 	entity->RenderEntity();
 	SDL_RenderPresent(renderer);
 	bool quit = false;
 	SDL_Event event;
 	while (!quit) {
+		int size = idQ.size();
+		for (int i = 0; i < size; i++) {
+			int id = idQ.front(); idQ.pop();
+			Entity* entity = entities.find(id)->second;
+			
+			SDL_Point p;
+			if (entity->v.empty()){
+				entity->flag = false;
+			}
+			else {
+				p = entity->v.back(); entity->v.pop_back();
+				entity->Animation(p);
+				idQ.push(id);
+			}
+		}
 
-		SDL_WaitEventTimeout(&event, 100);
+		SDL_WaitEventTimeout(&event, 1);
 		entity->Callback(event);
 	}
 	
