ff99da9f14865120307f5ca394429a8aee7c62f1
서상희 <tbvjaos510@naver.com>
2018-06-02 16:56:30 +0900

    socket 한글 리턴

--- server/main.js
+++ server/main.js
@@ -1,11 +1,22 @@
 var net = require('net');
 var iconv = require('iconv-lite');
 
+/**
+ * 
+ * @param {String} str utf-8로 인코딩 된 문자열
+ * @returns {String} euckr로 인코딩 된 문자열
+ */
+function toHangul(str){
+    return iconv.encode(str, 'euckr');
+}
+
 var server = net.createServer(function (client){
-    client.on('connect', function (){
-        console.log('Client connected');
-        
-    });
+    console.log('client generated');
+    
+    client.write(toHangul('한글'));
+    
 });
 
-server.listen(1331);
+server.listen(1331, function () {
+    console.log('TCP 서버 시작');    
+});
