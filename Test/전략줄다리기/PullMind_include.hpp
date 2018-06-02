1a6152e1cca35ea86bc5baff55464fdb68c1e18f
ShinSH0 <klkura1@naver.com>
2018-06-02 21:09:31 +0900

    대충 애니메이션 마무리?

--- Test/전략줄다리기/PullMind_include.hpp
+++ Test/전략줄다리기/PullMind_include.hpp
@@ -39,24 +39,112 @@
 
 using namespace std;
 
+class Entity;
 #define POWER_DEFAULT 1
 #define ENEMY 1
 #define ALLY 0
 #define TEAM_DEFAULT ALLY
 
 typedef vector<SDL_Point> VecP;
+typedef vector<Entity *> VEC_ENTI;
+typedef map<int, Entity *> MAP_ENTI;
 
+
+extern VEC_ENTI vec_enti;
+extern MAP_ENTI map_enti;
 extern queue<int> idQ;
 
-void PrintPoint(SDL_Point p, string str="") {
-	cout << str<<"	x: " << p.x << ", " << "y: " << p.y << endl;
+void PrintPoint(SDL_Point p, string str);
+bool compPoint(SDL_Point p1, SDL_Point p2);
+void PrintVecP(VecP v, string str);
+void PrintRect(SDL_Rect rect, string str );
+SDL_Rect Rect(int x, int y, int w, int h);
+void moveRect(SDL_Rect& rect, SDL_Point point);
+SDL_Point Point(int x, int y);
+void getPoints(VecP& v, SDL_Point p1, SDL_Point p2);
+
+
+class Entity {
+
+public:
+	SDL_Rect reg;
+	SDL_Rect src = { 0,0,0,0 }, dst;
+	SDL_Texture *img;
+	SDL_Renderer *renderer;
+	VecP v;
+	SDL_Point center, point= Point(-1, -1),point2;
+	int id;
+	int power;
+	bool team;
+	bool flag = false;
+		
+
+
+	Entity(SDL_Renderer * renderer,string filename, SDL_Rect dst, SDL_Rect reg, int id, int power=POWER_DEFAULT ,  bool team = TEAM_DEFAULT) {
+		this->power = power;
+		this->renderer = renderer;
+		this->team = team;
+		this->id = id;
+		this->img = IMG_LoadTexture(renderer, filename.c_str());
+		this->dst = dst;
+		this->reg = reg;
+		this->center = Point(dst.x + dst.w / 2, dst.y + dst.h / 2);
+		SDL_QueryTexture(this->img, NULL, NULL, &this->src.w, &this->src.h);
+		vec_enti.push_back(this);
+		map_enti.insert(make_pair(id, this));
+	}
+
+	void RenderEntity() {
+		SDL_RenderCopy(renderer, img, &src, &dst);
+	}
+	void PrintInfo() {
+		PrintPoint(this->center, "center");
+		PrintPoint(this->point, "point");
+		PrintPoint(this->point2, "point2");
+		PrintRect(this->reg, "reg");
+		PrintRect(this->src, "src");
+		PrintRect(this->dst, "dst");
+	}
+	void Callback(SDL_Event event) {
+		if (flag)
+			return;
+		switch (event.type) {
+		case SDL_MOUSEBUTTONDOWN:
+			point2 = Point(event.button.x, event.button.y);
+			if (SDL_PointInRect(&point2, &this->reg)) { //영역선택 시
+				cout << "영역" << endl;
+				this->point = this->center;
+			}
+			else if (!compPoint(point, Point(-1, -1))) {
+				cout << "영역바깥 그리고 -1,-1이 아님" << endl;
+				getPoints(v, this->point, point2);
+				reverse(v.begin(), v.end());
+				flag = true;
+				this->center = point2;
+				moveRect(this->reg, center);
+				this->point = Point(-1, -1);
+				idQ.push(id);
+			}
+		}
+	}
+	void Animation(SDL_Point p) {
+		
+			moveRect(this->dst, p);
+
+		
+	}
+
+};
+
+
+
+
+void PrintPoint(SDL_Point p, string str = "") {
+	cout << str << "	x: " << p.x << ", " << "y: " << p.y << endl;
+}
 bool compPoint(SDL_Point p1, SDL_Point p2) {
 
 	return p1.x == p2.x&&p1.y == p2.y;
-
-}
-void PrintPoint(SDL_Point p, string str = "") {
-	cout << str << "	x: " << p.x << ", " << "y: " << p.y << endl;
 
 }
 void PrintVecP(VecP v, string str = "") {
@@ -156,78 +244,5 @@ void getPoints(VecP& v, SDL_Point p1, SDL_Point p2)
 	}
 
 }
-class Entity {
-
-public:
-	SDL_Rect reg;
-	SDL_Rect src = { 0,0,0,0 }, dst;
-	SDL_Texture *img;
-	SDL_Renderer *renderer;
-	VecP v;
-	SDL_Point center, point= Point(-1, -1),point2;
-	int id;
-	int power;
-	bool team;
-	bool flag = false;
-		
-
-
-	Entity(SDL_Renderer * renderer,string filename, SDL_Rect dst, SDL_Rect reg, int id, int power=POWER_DEFAULT ,  bool team = TEAM_DEFAULT) {
-		this->power = power;
-		this->renderer = renderer;
-		this->team = team;
-		this->id = id;
-		this->img = IMG_LoadTexture(renderer, filename.c_str());
-		this->dst = dst;
-		this->reg = reg;
-		this->center = Point(dst.x + dst.w / 2, dst.y + dst.h / 2);
-		SDL_QueryTexture(this->img, NULL, NULL, &this->src.w, &this->src.h);
-	}
-
-	void RenderEntity() {
-		SDL_RenderCopy(renderer, img, &src, &dst);
-	}
-	void PrintInfo() {
-		PrintPoint(this->center, "center");
-		PrintPoint(this->point, "point");
-		PrintPoint(this->point2, "point2");
-		PrintRect(this->reg, "reg");
-		PrintRect(this->src, "src");
-		PrintRect(this->dst, "dst");
-	}
-	void Callback(SDL_Event event) {
-		if (flag)
-			return;
-		switch (event.type) {
-		case SDL_MOUSEBUTTONDOWN:
-			point2 = Point(event.button.x, event.button.y);
-			if (SDL_PointInRect(&point2, &this->reg)) { //영역선택 시
-				cout << "영역" << endl;
-				this->point = this->center;
-			}
-			else if (!compPoint(point, Point(-1, -1))) {
-				cout << "영역바깥 그리고 -1,-1이 아님" << endl;
-				getPoints(v, this->point, point2);
-				reverse(v.begin(), v.end());
-				idQ.push(id);
-				flag = true;
-				this->center = point2;
-				moveRect(this->reg, center);
-				this->point = Point(-1, -1);
-			}
-		}
-	}
-	void Animation(SDL_Point p) {
-		
-			moveRect(this->dst, p);
-			SDL_RenderClear(this->renderer);
-			RenderEntity();
-			SDL_RenderPresent(this->renderer);
-
-		
-	}
-
-
-};
 
 
--- Test/전략줄다리기/main.cpp
+++ Test/전략줄다리기/main.cpp
@@ -4,45 +4,63 @@
 #pragma comment (lib , "lib/SDL2.lib")
 #undef main 
 
+VEC_ENTI vec_enti;
+MAP_ENTI map_enti;
+
 queue<int> idQ;
 
 int main(void) {
 
-	
-	map<int, Entity *> entities;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window *window = SDL_CreateWindow("hi", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 
+	
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	Entity *entity = new Entity(renderer, "./resources/image/porg.jpg", Rect(100,100, 100,100),Rect(100,100,100,100), 1, 1, 1);
+	Entity *entity2 = new Entity(renderer, "./resources/image/porg.jpg", Rect(300, 100, 100, 100), Rect(300, 100, 100, 100), 2, 1, 1);
+	Entity *entity3 = new Entity(renderer, "./resources/image/porg.jpg", Rect(300, 300, 100, 100), Rect(300, 300, 100, 100), 3, 1, 1);
 
-	entities.insert(make_pair(entity->id, entity));
+
 	entity->RenderEntity();
+	entity2->RenderEntity();
+	entity3->RenderEntity();
 	SDL_RenderPresent(renderer);
 	bool quit = false;
 	SDL_Event event;
 	while (!quit) {
+		SDL_WaitEventTimeout(&event, 0);
+		SDL_RenderClear(renderer);
+		for (auto it = vec_enti.begin(); it != vec_enti.end(); it++) {
+			Entity* entity = (*it);
+			if(entity->flag==false) {
+				//애니메이션 중이 아님
+				entity->RenderEntity();
+			}
+		}
 		int size = idQ.size();
 		for (int i = 0; i < size; i++) {
 			int id = idQ.front(); idQ.pop();
-			Entity* entity = entities.find(id)->second;
-			
-			SDL_Point p;
-			if (entity->v.empty()){
+			Entity * entity = map_enti.find(id)->second;
+			if (entity->v.empty()) {
 				entity->flag = false;
 			}
 			else {
-				p = entity->v.back(); entity->v.pop_back();
+				SDL_Point p = entity->v.back(); entity->v.pop_back();
 				entity->Animation(p);
-				idQ.push(id);
 			}
+			entity->RenderEntity();
+			idQ.push(id);
 		}
 
-		SDL_WaitEventTimeout(&event, 1);
-		entity->Callback(event);
+	
+		
+		for (auto it = vec_enti.begin(); it != vec_enti.end(); it++) {
+			(*it)->Callback(event);
+		}
+		SDL_RenderPresent(renderer);
 	}
 	
 
