6cd0824546be1fae97918ff5afc12f84a731b41d
ShinSH0 <klkura1@naver.com>
2018-06-02 16:51:26 +0900

    PullMind_include 헤더 파일 추가

--- Test/전략줄다리기/PullMind_include.hpp
+++ Test/전략줄다리기/PullMind_include.hpp
@@ -13,12 +13,14 @@
 #include <direct.h>				//???? ???? ???
 #include <WinInet.h>
 #include <tchar.h>
+#include <string>
 #include "lib//iconv.h"
 #include "SDL/SDL.h"			//SDL - ?? ???????
 #include "SDL/SDL_image.h"		//SDL - ????? ???????
 #include "SDL/SDL_ttf.h"		//SDL - ???(????) ???????
 #include "SDL/han2unicode.h"	//han2unicode - ?? 949 ?????? uniocde?? ?????
-#include "SDL/SDL_mixer.h"		//SDL - ???? ???????
+#include "SDL/SDL_mixer.h"		//SDL - ???? ???????4
+
 
 #pragma comment (lib, "lib/libiconv.lib")
 #pragma comment (lib, "lib/SDL2")			//????? ??????? 1
@@ -31,6 +33,9 @@
 #pragma comment (lib, "wininet")
 
 
+using namespace std;
+
+
 typedef struct Entities {
 	Entity *entities;
 	int size;
@@ -38,12 +43,38 @@ typedef struct Entities {
 
 
 class Entity {
+
+private:
 	SDL_Rect reg;
 	SDL_Rect src, dst;
 	SDL_Texture *img;
+	SDL_Renderer *renderer;
 	int power;
 	int id;
 	bool team;
+
+	
+public:
+
+	Entity(string filename, SDL_Rect dst, int power, int id, bool team) {
+		this->power = power;
+		this->id = id;
+		this->team = team;
+		
+	}
+
+	void RenderEntity() {
+		SDL_RenderCopy(renderer, img, &src, &dst);
+	}
+
+
+
+};
+
+class My_Rect {
+
+private:
+	SDL_Rect rect;
 
 
 
