cb96fac7c973c8a3b49fa6cae1b882699722384e
starocean530 <soohan530@gmail.com>
2018-06-03 09:28:46 +0900

    ret

--- Test/전략줄다리기/main.cpp
+++ Test/전략줄다리기/main.cpp
@@ -31,21 +31,20 @@ void ReceiveHandler(void) {
 		//printf("%s\n", msg);
 		if (msg[3] == 'e' && msg[2] == 'v') {
 
-			sscanf(msg, "move %1d %2d %4d,%4d %4d,%4d", &buffint, &e_num, &p1.x, &p1.y, &p2.x, &p2.y);
-			
-			if (buffint != my_idx) {
+			sscanf(msg, "move %d %d %d %d %d %d", &buffint, &e_num, &p1.x, &p1.y, &p2.x, &p2.y);
+			if (buffint != my_idx)
+				continue;
 				if (e_num - 6 <= 8) {
-					e_num = 36- e_num;
+					e_num = 36 - e_num;
 				}
 				else {
-					e_num = 51-e_num;
+					e_num = 51 - e_num;
 				}
 
 				p1.x = 1920 - p1.x;
-				p2.x = 1920 - p2.x;
+				p2.x = 1920 - p2.x;		
 
-			}
-			printf("data : %d %d %d %d %d %d\n", buffint, e_num, p1.x, p1.y, p2.x, p2.y);
+			printf("processed data : %d %d %d %d %d %d\n", buffint, e_num, p1.x, p1.y, p2.x, p2.y);
 			moveEntity(e_num, p1, p2);
 		}
 		else if (strstr(msg, "match ") != NULL) {
